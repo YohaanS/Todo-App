2a3b97f8b50c69e423f3cf8516af69e9e0ed281a
YohaanS <yohaan.singla@gmail.com>
2023-04-10 22:21:58 -0700

    Added some things

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,17 @@
 import { Box } from '@mantine/core'
+import useSWR from 'swr'; 
 import './App.css'
 
-function App() {
-  
+export const ENDPOINT = "http://localhost:4000"
 
-  return (
-    <Box></Box>
-  )
+const fetcher = (url: string) => fetch(`${ENDPOINT}/${url}`).then((r) => r.json()); 
+
+function App() {
+
+  const {data, mutate} = useSWR('api/todos', fetcher)
+
+  return <Box>{JSON.stringify(data)}</Box>
+  
 }
 
 export default App
