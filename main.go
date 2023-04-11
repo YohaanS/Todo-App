794b80aa553cc941e85d18c162e48060cb6c9c8a
YohaanS <yohaan.singla@gmail.com>
2023-04-10 19:38:18 -0700

    Added patch request and get request

--- main.go
+++ main.go
@@ -19,7 +19,6 @@ func main() {
 
 	app := fiber.New()
 
-
 	todos := []Todo{}
 
 	app.Get("/healthcheck", func(c *fiber.Ctx) error {
@@ -29,18 +28,37 @@ func main() {
 	app.Post("/todos", func(c *fiber.Ctx) error {
 		todo := &Todo{}
 
-		 if err := c.BodyParser(todo); err != nil {
+		if err := c.BodyParser(todo); err != nil {
 			return err
-		 }
+		}
 
-		 todo.ID = len(todos) + 1 
+		todo.ID = len(todos) + 1
 
-		 todos = append(todos, *todo)
+		todos = append(todos, *todo)
 
-		 return c.JSON(todos)
+		return c.JSON(todos)
 	})
 
+	app.Patch("/todos/:id/done", func(c *fiber.Ctx) error {
+		id, err := c.ParamsInt("id")
 
-	
+		if err != nil {
+			return c.Status(401).SendString("Invalid id")
+		}
+
+		for i, t := range todos {
+			if t.ID == id {
+				todos[i].Done = true
+				break
+			}
+		}
+
+		return c.JSON(todos)
+	})
+
+	app.Get("/todos", func(c *fiber.Ctx) error {
+		return c.JSON(todos)
+	})
+
 	log.Fatal(app.Listen(":3000"))
 }
