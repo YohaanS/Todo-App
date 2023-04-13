e56494c2c8960784e9dd374082f262d49eae7627
YohaanS <yohaan.singla@gmail.com>
2023-04-12 18:00:37 -0700

    Updated Todo client components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,71 @@
-import { Box } from '@mantine/core'
-import useSWR from 'swr'; 
-import './App.css'
+import { Box, List, ThemeIcon } from "@mantine/core";
+import useSWR from "swr";
+import AddTodo from "./components/AddTodo";
+import { CheckCircleFillIcon } from "@primer/octicons-react";
 
-export const ENDPOINT = "http://localhost:4000"
 
-const fetcher = (url: string) => fetch(`${ENDPOINT}/${url}`).then((r) => r.json()); 
-
-function App() {
-
-  const {data, mutate} = useSWR('api/todos', fetcher)
-
-  return <Box>{JSON.stringify(data)}</Box>
-  
+export interface Todo {
+  id: number;
+  title: string;
+  body: string;
+  done: boolean;
 }
 
-export default App
+export const ENDPOINT = "http://localhost:4000/api";
+
+const fetcher = (url: string) =>
+  fetch(`${ENDPOINT}/${url}`).then((res) => res.json());
+
+function App() {
+  const { data, mutate } = useSWR<Todo[]>("todos", fetcher);
+
+  async function markTodoAsDone(id: number) {
+    const updated = await fetch(`${ENDPOINT}/todos/${id}/done`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((r) => r.json());
+
+    mutate(updated);
+  }
+
+  return (
+    <Box
+      sx={(theme) => ({
+        padding: "2rem",
+        width: "100%",
+        maxWidth: "40rem",
+        margin: "0 auto",
+      })}
+    >
+      <List spacing="xs" size="sm" mb={12} center>
+        {data?.map((todo) => {
+          return (
+            <List.Item
+              onClick={() => markTodoAsDone(todo.id)}
+              key={`todo__${todo.id}`}
+              icon={
+                todo.done ? (
+                  <ThemeIcon color="teal" radius="xl" size={24}>
+                    <CheckCircleFillIcon size={20} />
+                  </ThemeIcon>
+                ) : (
+                  <ThemeIcon color="gray" radius="xl" size={24}>
+                    <CheckCircleFillIcon size={20} />
+                  </ThemeIcon>
+                )
+              }
+            >
+              {todo.title}
+            </List.Item>
+          );
+        })}
+      </List>
+
+      <AddTodo mutate={mutate}></AddTodo>
+    </Box>
+  );
+}
+
+export default App;
